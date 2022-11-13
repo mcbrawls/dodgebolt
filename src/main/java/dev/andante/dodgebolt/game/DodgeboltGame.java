19ab3e5580a403dfca526205e96d5cdfd76aef9b
andantet <orrdaniel73@gmail.com>
2022-11-13 12:47:16 +0000

    make return to your own half text bold

--- src/main/java/dev/andante/dodgebolt/game/DodgeboltGame.java
+++ src/main/java/dev/andante/dodgebolt/game/DodgeboltGame.java
@@ -207,7 +207,7 @@ public class DodgeboltGame {
                         double z = player.getZ();
                         if (isAlpha ? z >= ARENA_MID_Z : z <= ARENA_MID_Z) {
                             TitleHelper.sendTimes(player, 0, 5, 0);
-                            TitleHelper.sendTitle(player, Text.empty(), Text.literal("<< RETURN TO YOUR HALF >>").formatted(Formatting.RED));
+                            TitleHelper.sendTitle(player, Text.empty(), Text.literal("<< RETURN TO YOUR HALF >>").formatted(Formatting.BOLD, Formatting.RED));
                             player.damage(DamageSource.IN_WALL, 2.0F);
                             player.getInventory().remove(stack -> stack.isOf(Items.BOW), -1, player.playerScreenHandler.getCraftingInput());
                         } else {
